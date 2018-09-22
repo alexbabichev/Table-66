e3c35bc7c3b35908dac4ef7fe5b1678e9946edc4
Mikhail Gorbachev <mgorbachev@factset.com>
2018-09-22 18:39:33 +0100

    flushed out generate id page screen

--- src/pages/generate-proof/generate-proof.ts
+++ src/pages/generate-proof/generate-proof.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-
-
-/**
- * Generated class for the GenerateProofPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-generate-proof',
@@ -23,11 +14,18 @@ export class GenerateProofPage {
     address: false,
   };
 
+  public idImage = 'resources/sample-id-card.jpg';
+  public isSubmitted = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GenerateProofPage');
+  }
+
+  generateQRCode() {
+    this.isSubmitted = true;
   }
 
 }
