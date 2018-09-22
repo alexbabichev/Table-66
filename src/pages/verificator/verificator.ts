efc68741bbf5b9cd576c1cc59b3cb29731201eda
Mikhail Gorbachev <mgorbachev@factset.com>
2018-09-22 20:02:58 +0100

    added photo display from base64 in verificator

--- src/pages/verificator/verificator.ts
+++ src/pages/verificator/verificator.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import image from './image';
 /**
  * Generated class for the VerificatorPage page.
  *
@@ -15,11 +16,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VerificatorPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private _placeHolderSafe: SafeUrl;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private sanitizer: DomSanitizer) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VerificatorPage');
   }
 
+  public ngOnInit() {
+    this._placeHolderSafe = this.sanitizer.bypassSecurityTrustUrl(image);
+  }
+
+  public get placeholder() {
+    return this._placeHolderSafe;
+  }
 }
