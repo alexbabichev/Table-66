cf2050345a045ba9cf4719cba81320232dc11be0
Alex Babichev <alex.babichev@me.com>
2018-09-22 17:31:11 +0100

    QR generator

--- src/pages/generate-proof/generate-proof.module.ts
+++ src/pages/generate-proof/generate-proof.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { GenerateProofPage } from './generate-proof';
 
+import { QRCodeModule } from 'angularx-qrcode';
+
 @NgModule({
   declarations: [
     GenerateProofPage,
   ],
   imports: [
+    QRCodeModule,
     IonicPageModule.forChild(GenerateProofPage),
   ],
 })
