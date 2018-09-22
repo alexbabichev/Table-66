2e131a55dac5a6a70833eec5dd9af51a6f7cff7f
Mikhail Gorbachev <mgorbachev@factset.com>
2018-09-22 19:03:19 +0100

    added upload-id page mapping

--- src/pages/upload-id/upload-id.ts
+++ src/pages/upload-id/upload-id.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
-
-/**
- * Generated class for the UploadIdPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-upload-id',
@@ -17,13 +8,16 @@ import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 })
 export class UploadIdPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private qrScanner: QRScanner) {
-    this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        console.log(status);
-      });
-  }
+  public uploadData = {
+    identityId: '',
+    fullName: '',
+    dateOfBirth: '',
+    address: '',
+  };
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UploadIdPage');
