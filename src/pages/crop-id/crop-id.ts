f11d2c0465dd41bedf181e01792fc2a3726b553e
Mikhail Gorbachev <mgorbachev@factset.com>
2018-09-22 22:20:19 +0100

    podkrutil

--- src/pages/crop-id/crop-id.ts
+++ src/pages/crop-id/crop-id.ts
@@ -1,7 +1,7 @@
 //our root app component
-import {Component, NgModule, ViewChild} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
-import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
+import { Component, NgModule, ViewChild } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { ImageCropperComponent, CropperSettings, Bounds } from 'ng2-img-cropper';
 
 @IonicPage()
 @Component({
@@ -10,54 +10,57 @@ import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
 })
 
 export class CropIdPage {
-    name:string;
-    data1:any;
-    cropperSettings1:CropperSettings;
-    croppedWidth:number;
-    croppedHeight:number;
+  name: string;
+  data1: any;
+  cropperSettings1: CropperSettings;
+  croppedWidth: number;
+  croppedHeight: number;
 
-    @ViewChild('cropper', undefined) cropper:ImageCropperComponent;
+  @ViewChild('cropper', undefined) cropper: ImageCropperComponent;
 
-    constructor() {
-      this.name = 'Angular2'
-      this.cropperSettings1 = new CropperSettings();
-      this.cropperSettings1.width = 200;
-      this.cropperSettings1.height = 200;
+  constructor() {
+    this.cropperSettings1 = new CropperSettings();
+    this.cropperSettings1.width = 150;
+    this.cropperSettings1.height = 200;
 
-      this.cropperSettings1.croppedWidth = 200;
-      this.cropperSettings1.croppedHeight = 200;
+    this.cropperSettings1.croppedWidth = 150;
+    this.cropperSettings1.croppedHeight = 200;
 
-      this.cropperSettings1.canvasWidth = 500;
-      this.cropperSettings1.canvasHeight = 300;
+    this.cropperSettings1.canvasWidth = 500;
+    this.cropperSettings1.canvasHeight = 300;
 
-      this.cropperSettings1.minWidth = 10;
-      this.cropperSettings1.minHeight = 10;
+    this.cropperSettings1.minWidth = 10;
+    this.cropperSettings1.minHeight = 10;
 
-      this.cropperSettings1.rounded = false;
-      this.cropperSettings1.keepAspect = false;
+    this.cropperSettings1.rounded = false;
+    this.cropperSettings1.keepAspect = true;
 
-      this.cropperSettings1.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
-      this.cropperSettings1.cropperDrawSettings.strokeWidth = 2;
+    this.cropperSettings1.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
+    this.cropperSettings1.cropperDrawSettings.strokeWidth = 2;
 
-      this.data1 = {};
+    this.data1 = {};
   }
 
-  cropped(bounds:Bounds) {
-    this.croppedHeight =bounds.bottom-bounds.top;
-    this.croppedWidth = bounds.right-bounds.left;
+  cropped(bounds: Bounds) {
+    this.croppedHeight = bounds.bottom - bounds.top;
+    this.croppedWidth = bounds.right - bounds.left;
   }
 
   fileChangeListener($event) {
-    var image:any = new Image();
-    var file:File = $event.target.files[0];
-    var myReader:FileReader = new FileReader();
+    var image: any = new Image();
+    var file: File = $event.target.files[0];
+    var myReader: FileReader = new FileReader();
     var that = this;
-    myReader.onloadend = function (loadEvent:any) {
-        image.src = loadEvent.target.result;
-        that.cropper.setImage(image);
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
 
     };
 
     myReader.readAsDataURL(file);
-    }
+  }
+
+  uploadCroppedPhoto() {
+
+  }
 }
